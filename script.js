951bd102d1a37ca2a1efce1ee69e09dd57096853
Lyr-a <estevaoturbay@gmail.com>
2023-05-23 16:28:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function modal(){
+
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'block';
+
+}
+
+function fmodal(){
+
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'none';
+
+}
