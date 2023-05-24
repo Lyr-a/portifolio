881f6f38d36d3c2cd5897bb63d8c48d8df24824b
Lyr-a <estevaoturbay@gmail.com>
2023-05-24 15:55:45 -0300

    tentando ajeitar os contatatos na janela modal, configurei o click do gmail

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function fmodal(){
     modal.style.display = 'none';
 
 }
+
+function copiargmail() {
+
+    navigator.clipboard.writeText("estevaoturbay@gmail.com");
+  
+    // Alert the copied text
+    alert("Gmail copiado para a área de Transferência");
+  }
