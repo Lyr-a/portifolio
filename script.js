d06a3850d639b8350a582cfd2c681a7c0e8131c8
Lyr-a <estevaoturbay@gmail.com>
2023-05-30 02:33:02 -0300

    ajeitando o display do postifólio, em breve estará funcionando

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*import Typed from `https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js`
+
+
+var typed = new Typed(".auto-type", {
+    strings: ["meu nome é Estêvão."],
+    typeSpeed: 70,
+    backSpeed: 140,
+    loop: false
+})
+*/      
+
+const boxWrapper = document.getElementById("lista");
+
+for (var i = 0; i < 9; i++) {
+    
+    const box = document.createElement("div");
+    box.classList.add("box");
+    boxWrapper.appendChild(box)
+}
+
 function modal(){
 
     let modal = document.querySelector('.modal')
@@ -21,3 +41,14 @@ function copiargmail() {
     // Alert the copied text
     alert("Gmail copiado para a área de Transferência");
   }
+
+
+
+
+
+
+
+
+
+
+
